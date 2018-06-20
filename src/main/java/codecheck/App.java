3f0a2bae439e5fa2fea737f832b31c46e60b50ce
akiyama_kazuhiko <akiyama_kazuhiko@192.168.11.5>
2018-06-20 22:12:38 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -6,24 +6,29 @@ public class App {
 	public static void main(String[] args) {
 		LinkedHashMap checkMap = new LinkedHashMap();
 		long target = 0;
+		String targetStr = "";
 		for (int i = 0, l = args.length; i < l; i++) {
 			if (args[i].contains(":")) {
 				String[] ar = args[i].split(":");
 				checkMap.put(Long.parseLong(ar[0]), ar[1]);
-			}else {
+			} else {
 				target = Long.parseLong(args[i]);
+				targetStr = args[i];
 			}
 		}
 		String output = "";
 
 		for (Object key : checkMap.keySet()) {
-			if(target % (long)key == 0) {
+			if (target % (long) key == 0) {
 				output = output + checkMap.get(key);
 			}
 		}
+		if ("".equals(output)) {
+			output = targetStr;
+		}
 		System.out.println(output);
-//		for(int i = 0, l = checkMap.size(); i < l; i++) {
-//			if(target % checkMap.get(key))
-//		}
+		//		for(int i = 0, l = checkMap.size(); i < l; i++) {
+		//			if(target % checkMap.get(key))
+		//		}
 	}
 }
