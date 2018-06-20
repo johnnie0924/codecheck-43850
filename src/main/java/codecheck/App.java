c02d8caf17d67717323e9ce9b2d8ee569492b163
akiyama_kazuhiko <akiyama_kazuhiko@192.168.11.5>
2018-06-20 22:02:58 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,28 @@
 package codecheck;
 
+import java.util.LinkedHashMap;
+
 public class App {
 	public static void main(String[] args) {
+		LinkedHashMap checkMap = new LinkedHashMap();
+		long target = 0;
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+			if (args[i].contains(":")) {
+				String[] ar = args[i].split(":");
+				checkMap.put(ar[0], ar[1]);
+			}else {
+				target = Long.parseLong(args[i]);
+			}
 		}
+		String output = "";
+
+		for (Object key : checkMap.keySet()) {
+			if(target % (long)key == 0) {
+				output = output + checkMap.get(key);
+			}
+		}
+//		for(int i = 0, l = checkMap.size(); i < l; i++) {
+//			if(target % checkMap.get(key))
+//		}
 	}
 }
